eb043f3a3ce36aadbf9cc67f3945c6e81cb60d8e
Alexis Fernandez <99295832+alexis-fernandez@users.noreply.github.com>
2024-01-25 21:20:54 -0500

    Update OF.c

--- OF.c
+++ OF.c
@@ -1,11 +1,10 @@
 #include <math.h>
 #include "functions.h"
-// Do know change anything in this file. You will not submit this file.
 
-// Define the objective function function for optimization
+
 double Objective_function(int NUM_VARIABLES, double x[NUM_VARIABLES])
 {
-    // While the objective function could be anything here it is to minimize Ackley function
+    //minimize the Akley function
     double sum1 = 0.0, sum2 = 0.0;
     for (int i = 0; i < NUM_VARIABLES; i++)
     {
